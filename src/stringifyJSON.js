eae094aa4ae9424e50a9d0967b50131003e5f8f5
helloitjase <jason.sl.chen@gmail.com>
2019-07-15 11:53:27 -0700

    covered primitive data types and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,20 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    let resultsArray = [];
+    for (let i = 0; i < obj.length; i++) {
+      resultsArray.push(stringifyJSON(obj[i]));
+    }
+    let results = resultsArray.join(', ')
+    return '[' + results + ']';
+  }
+
+
 };
+
