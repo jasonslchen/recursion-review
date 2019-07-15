35eafe1dd6c1fd2f9b7d830ee6a811c725e600f0
helloitjase <jason.sl.chen@gmail.com>
2019-07-15 16:13:04 -0700

    finish getElementsbyClassName test suites

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,22 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  let elementsArray = [];
+
+  var getElements = function(nodes) {
+    if (nodes.classList && nodes.classList.length !== 0 && nodes.classList.contains(className)) {
+
+      elementsArray.push(nodes);
+    }
+
+    if (nodes.childNodes.length !== 0) {
+      for (let i = 0; i < nodes.childNodes.length; i++) {
+        getElements(nodes.childNodes[i]);
+      }
+    }
+  };
+
+  getElements(document.body);
+
+  return elementsArray;
 };
